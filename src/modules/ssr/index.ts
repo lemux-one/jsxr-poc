5e297b7c518b5db6978362ba74f21a5d28bde8fd
Luis Lemus <lemux.one@gmail.com>
2024-11-10 16:05:24 -0500

    added SSR module with tests

---
+++ src/modules/ssr/index.ts
@@ -0,0 +1,42 @@
+function isSelfClosingTag(tag: string) {
+  return ["br", "img", "input", "hr", "meta", "link"].includes(tag);
+}
+
+function renderAttrs(attrs: any) {
+  return attrs
+    ? Object.keys(attrs)
+        .map((attrName) => ` ${attrName}="${attrs[attrName]}"`)
+        .join("")
+    : "";
+}
+
+function renderChildren(children: unknown) {
+  if (!children) return "";
+  if (Array.isArray(children)) return children.map(html).join("");
+  return html(children);
+}
+
+export function html(node: any): string {
+  if (!node) return "";
+  if (typeof node === "string") return `${node}`;
+  if (Array.isArray(node)) return node.map(html).join("");
+  if (node.tag === null) {
+    return renderChildren(node.children); // Fragment: only children matter
+  }
+  if (node.tag) {
+    const { tag, attrs = {}, children } = node;
+    if (typeof tag === "function") {
+      return tag.name === "jsxFragmentHandler"
+        ? html(tag(children)) // Fragment
+        : html(tag({ ...attrs, children })); // FC
+    }
+    const doctype = tag === "html" ? "<!DOCTYPE html>" : "";
+    return isSelfClosingTag(tag)
+      ? `${doctype}<${tag}${renderAttrs(attrs)}/>`
+      : `${doctype}<${tag}${renderAttrs(attrs)}>${renderChildren(
+          children
+        )}</${tag}>`;
+  }
+  console.error(`Unsupported node: ${JSON.stringify(node)}`);
+  return "";
+}
---
+++ src/modules/ssr/ssr.test.tsx
@@ -0,0 +1,74 @@
+import { test, expect } from "bun:test";
+import { html } from ".";
+
+test("empty fragment", () => {
+  const rendered = html(<></>);
+  expect(rendered).toEqual("");
+});
+
+test("fragment with children", () => {
+  const rendered = html(
+    <>
+      <span>Dummy</span>
+    </>
+  );
+  expect(rendered).toEqual("<span>Dummy</span>");
+});
+
+test("fragment with children having props", () => {
+  const rendered = html(
+    <>
+      <p class="txt">Dummy</p>
+    </>
+  );
+  expect(rendered).toEqual('<p class="txt">Dummy</p>');
+});
+
+test("fragment with nested children having props and using self closing html tags", () => {
+  const rendered = html(
+    <>
+      <span class="greeting">
+        Hello
+        <br />
+        Bun!
+      </span>
+      <p class="txt">Dummy p</p>
+    </>
+  );
+  expect(rendered).toEqual(
+    '<span class="greeting">Hello<br/>Bun!</span><p class="txt">Dummy p</p>'
+  );
+});
+
+test("span with no props and no children", () => {
+  const rendered = html(<span></span>);
+  expect(rendered).toEqual("<span></span>");
+});
+
+test("span with class and text children", () => {
+  const rendered = html(<span class="txt">This is a text</span>);
+  expect(rendered).toEqual('<span class="txt">This is a text</span>');
+});
+
+test("doctype prefix", () => {
+  const rendered = html(<html lang="eng"></html>);
+  expect(rendered).toEqual('<!DOCTYPE html><html lang="eng"></html>');
+});
+
+test("simple component with props and no children", () => {
+  function SimpleComponent(props: { class: string }) {
+    return <p {...props}>Dummy text</p>;
+  }
+  expect(html(<SimpleComponent class="simple" />)).toEqual(
+    '<p class="simple">Dummy text</p>'
+  );
+});
+
+test("simple component with only children props", () => {
+  function WrapperComponent(props: any) {
+    return <p>{props.children}</p>;
+  }
+  expect(html(<WrapperComponent>Child</WrapperComponent>)).toEqual(
+    "<p>Child</p>"
+  );
+});
