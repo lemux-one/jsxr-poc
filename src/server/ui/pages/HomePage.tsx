0545fd3d8478543133f44616addbc32933d17e85
Luis Lemus <lemux.one@gmail.com>
2024-11-28 13:44:46 -0500

    added more logic to home page in order to use all the new features in a meaningful way

--- src/server/ui/pages/HomePage.tsx
+++ src/server/ui/pages/HomePage.tsx
@@ -1,14 +1,29 @@
-import Show from "../helpers/Show";
 import { BaseLayout } from "../layouts/BaseLayout";
+import { For, Show } from "../helpers";
 
 export function HomePage() {
+  const todos = ["Try Tailwind 4", "Post an article to my blog"];
+  const userName = "Sir";
+  const isMorning = new Date().toLocaleTimeString().split(" ")[1] === "AM";
   return (
     <BaseLayout title="Home Page">
-      <h1 class="font-bold text-[2rem]">Home Page</h1>
-      <Show if={true} else={<p>Hello, Sir!</p>}>
-        <p>Good morning, Sir!</p>
-      </Show>
-      <p>Welcome back!</p>
+      <main class="p-[1rem]">
+        <h1 class="font-bold text-[2rem] text-primary">Home Page</h1>
+        <Show if={isMorning} else={<p>Hello, {userName}!</p>}>
+          <p>Good morning, {userName}!</p>
+        </Show>
+        <p>Welcome back!</p>
+        <h2 class="mt-[1.5rem] text-[1.2rem] underline">Your ToDo list:</h2>
+        <ul class="ml-[1rem]">
+          <For each={todos}>
+            {(todo: string) => (
+              <li class="before:content-['-'] before:text-foreground before:mr-[.3rem]">
+                <span>{todo}</span>
+              </li>
+            )}
+          </For>
+        </ul>
+      </main>
     </BaseLayout>
   );
 }
