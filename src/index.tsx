b9efb86fb0b74ca9bef42fe47d55c6f77cdd4a20
Luis Lemus <lemux.one@gmail.com>
2024-11-29 10:14:13 -0500

    (wip) added logic to handle post request to update todos

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
-import { isGet, start } from "httpw";
-import { env } from "./server/utils";
+import { handlePost, isGetOrPost, isPost, start } from "httpw";
+import { env, storage } from "./server/utils";
 import { HomePage } from "./server/ui/pages/HomePage";
 import { staticHandler } from "./server/handlers";
+
+storage().users = [{ id: 1, name: "Lemurio" }];
+storage().todos = [
+  { id: 1, desc: "Try Tailwind 4 by building something", done: true },
+  { id: 2, desc: "Post an article about it on my blog", done: false },
+];
 
 start({
   port: env().port,
@@ -9,9 +15,28 @@ start({
   handlers: [
     staticHandler,
     {
-      accepts: (req) => isGet(req) && req.url === "/",
+      accepts: (req) => isGetOrPost(req) && req.url === "/",
       handle(c) {
-        c.html(<HomePage />);
+        if (isPost(c.req)) {
+          handlePost(c.req, (data) => {
+            const { _type, ...todo } = data;
+            if (_type === "todo") {
+              storage().todos = [...storage().todos].map((current: any) => {
+                if (String(current.id) === todo.id) {
+                  return {
+                    ...current,
+                    desc: todo.desc,
+                    done: todo.done === "on",
+                  };
+                }
+                return current;
+              });
+            }
+            c.html(<HomePage />);
+          });
+        } else {
+          c.html(<HomePage />);
+        }
       },
     },
   ],
--- src/server/ui/pages/HomePage.tsx
+++ src/server/ui/pages/HomePage.tsx
@@ -1,27 +1,40 @@
 import { BaseLayout } from "../layouts/BaseLayout";
 import { For, Show } from "../helpers";
+import { storage } from "../../utils";
 
 interface ITodo {
+  id: number;
   desc: string;
   done?: boolean;
 }
 
 function Todo(props: ITodo) {
-  const { desc, done } = props;
+  const { id, desc, done } = props;
   return (
-    <li>
-      <input type="checkbox" checked={done} />
-      <span class="ml-1">{desc}</span>
+    <li class="p-1.5">
+      <form action="/" method="post" name={`todo-${id}`}>
+        <input type="hidden" name="_type" value="todo" />
+        <input type="hidden" name="id" value={id} />
+        <input type="checkbox" name="done" checked={done} />
+        <input
+          class="ml-1 w-96 bg-bg border border-gray-500 pt-1 pb-2 px-2"
+          name="desc"
+          value={desc}
+        />
+        <button
+          class="ml-2 bg-fg text-bg px-1.5 pb-1 cursor-pointer hover:underline"
+          type="submit"
+        >
+          Save
+        </button>
+      </form>
     </li>
   );
 }
 
 export function HomePage() {
-  const todos = [
-    { desc: "Try Tailwind 4 by building something", done: true },
-    { desc: "Post an article about it on my blog", done: false },
-  ];
-  const userName = "Sir";
+  const todos: ITodo[] = storage().todos;
+  const userName = storage().users[0].name;
   const isMorning = new Date().toLocaleTimeString().split(" ")[1] === "AM";
   return (
     <BaseLayout title="Home Page">
