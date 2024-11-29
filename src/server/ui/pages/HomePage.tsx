f60d8b2752f82626b6a7ae25d137e90c6da60ba3
Luis Lemus <lemux.one@gmail.com>
2024-11-28 23:02:45 -0500

    (wip) extended todo definition and using a custom compoent to render

--- src/server/ui/pages/HomePage.tsx
+++ src/server/ui/pages/HomePage.tsx
@@ -1,27 +1,39 @@
 import { BaseLayout } from "../layouts/BaseLayout";
 import { For, Show } from "../helpers";
 
+interface ITodo {
+  desc: string;
+  done?: boolean;
+}
+
+function Todo(props: ITodo) {
+  const { desc, done } = props;
+  return (
+    <li>
+      <input type="checkbox" checked={done} />
+      <span class="ml-1">{desc}</span>
+    </li>
+  );
+}
+
 export function HomePage() {
-  const todos = ["Try Tailwind 4", "Post an article to my blog"];
+  const todos = [
+    { desc: "Try Tailwind 4 by building something", done: true },
+    { desc: "Post an article about it on my blog", done: false },
+  ];
   const userName = "Sir";
   const isMorning = new Date().toLocaleTimeString().split(" ")[1] === "AM";
   return (
     <BaseLayout title="Home Page">
-      <main class="p-[1rem]">
-        <h1 class="font-bold text-[2rem] text-primary">Home Page</h1>
+      <main class="p-5">
+        <h1 class="font-bold text-2xl text-primary">Home Page</h1>
         <Show if={isMorning} else={<p>Hello, {userName}!</p>}>
           <p>Good morning, {userName}!</p>
         </Show>
         <p>Welcome back!</p>
-        <h2 class="mt-[1.5rem] text-[1.2rem] underline">Your ToDo list:</h2>
-        <ul class="ml-[1rem]">
-          <For each={todos}>
-            {(todo: string) => (
-              <li class="before:content-['-'] before:text-foreground before:mr-[.3rem]">
-                <span>{todo}</span>
-              </li>
-            )}
-          </For>
+        <h2 class="mt-5 text-lg underline">Your ToDo list:</h2>
+        <ul class="ml-3">
+          <For each={todos}>{(todo: ITodo) => <Todo {...todo} />}</For>
         </ul>
       </main>
     </BaseLayout>
