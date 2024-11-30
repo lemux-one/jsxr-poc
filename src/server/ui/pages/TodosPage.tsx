c0da521a7117568d71215e9a49a80067791f1666
Luis Lemus <lemux.one@gmail.com>
2024-11-30 10:58:06 -0500

    created common folder and cleaned TodosPage code

--- src/server/ui/pages/TodosPage.tsx
+++ src/server/ui/pages/TodosPage.tsx
@@ -2,26 +2,19 @@ import { storage } from "../../utils";
 import { For } from "../helpers";
 import { BaseLayout } from "../layouts/BaseLayout";
 
-interface ITodo {
-  id: number;
-  desc: string;
-  done?: boolean;
-}
-
-function Todo(props: ITodo) {
-  const { id, desc, done } = props;
+function Todo({ todo }: { todo: ITodo }) {
   return (
     <li class="p-1.5">
-      <input type="checkbox" name={`${id}-done`} checked={done} />
+      <input type="checkbox" name={`${todo.id}-done`} checked={todo.done} />
       <input
         class="ml-1 w-96 bg-bg border border-gray-500 pt-1 pb-2 px-2"
-        name={`${id}-desc`}
-        value={desc}
+        name={`${todo.id}-desc`}
+        value={todo.desc}
       />
       <button
         class="ml-2 bg-fg text-bg px-1.5 pb-1 cursor-pointer hover:underline"
         type="submit"
-        formaction={`/?todo=${id}`}
+        formaction={`/?todo=${todo.id}`}
       >
         Save
       </button>
@@ -31,15 +24,16 @@ function Todo(props: ITodo) {
 
 export function TodosPage() {
   const todos: ITodo[] = storage().todos;
+  const userName = storage().users[0].name;
 
   return (
     <BaseLayout title="Home Page">
       <main class="p-5">
-        <h2 class="mt-5 text-lg underline">Your ToDo list:</h2>
+        <h2 class="text-lg">{userName}'s ToDo list:</h2>
         <form action="/" method="post" name={`todos`}>
           <input type="hidden" name="_type" value="todos" />
           <ul class="ml-3">
-            <For each={todos}>{(todo: ITodo) => <Todo {...todo} />}</For>
+            <For each={todos}>{(todo: ITodo) => <Todo todo={todo} />}</For>
           </ul>
           <button
             class="ml-2 bg-fg text-bg px-1.5 pb-1 cursor-pointer hover:underline"
